014ccb75610585770a6378c870d744a4f684c665
Piotr Krzeminski <git@krzeminski.it>
2022-05-07 14:38:01 +0200

    Use Webpack 5, add header

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -5,6 +5,7 @@ import org.jetbrains.compose.web.css.padding
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.Button
 import org.jetbrains.compose.web.dom.Div
+import org.jetbrains.compose.web.dom.H1
 import org.jetbrains.compose.web.dom.Span
 import org.jetbrains.compose.web.dom.Text
 import org.jetbrains.compose.web.renderComposable
@@ -13,6 +14,9 @@ fun main() {
     var count: Int by mutableStateOf(0)
 
     renderComposable(rootElementId = "root") {
+        H1 {
+            Text("Counter")
+        }
         Div({ style { padding(25.px) } }) {
             Button(attrs = {
                 onClick { count -= 1 }
