5c92e3e5a051f9ca0bebe36eae533fa94d9e2e95
Piotr Krzeminski <git@krzeminski.it>
2022-05-07 14:51:09 +0200

    Create a composable

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -1,6 +1,11 @@
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import org.jetbrains.compose.web.css.Color
+import org.jetbrains.compose.web.css.color
+import org.jetbrains.compose.web.css.fontWeight
 import org.jetbrains.compose.web.css.padding
 import org.jetbrains.compose.web.css.px
 import org.jetbrains.compose.web.dom.Button
@@ -11,28 +16,66 @@ import org.jetbrains.compose.web.dom.Text
 import org.jetbrains.compose.web.renderComposable
 
 fun main() {
-    var count: Int by mutableStateOf(0)
+    var countInMainFunction: Int by mutableStateOf(0)
 
     renderComposable(rootElementId = "root") {
         H1 {
             Text("Counter")
         }
-        Div({ style { padding(25.px) } }) {
+        Div({
+            style {
+                padding(25.px)
+                color(Color.red)
+                fontWeight("bold")
+            }
+        }) {
             Button(attrs = {
-                onClick { count -= 1 }
+                onClick { countInMainFunction -= 1 }
             }) {
                 Text("-")
             }
 
             Span({ style { padding(15.px) } }) {
-                Text("$count")
+                Text("$countInMainFunction")
             }
 
             Button(attrs = {
-                onClick { count += 1 }
+                onClick { countInMainFunction += 1 }
             }) {
                 Text("+")
             }
+        }
+
+        Counter()
+    }
+}
+
+@Composable
+fun Counter() {
+    // TODO explain: https://developer.android.com/jetpack/compose/state says that remember { ... } is needed, and it
+    // works well without it. Why?
+    // Read:
+    // - https://medium.com/androiddevelopers/understanding-jetpack-compose-part-1-of-2-ca316fe39050
+    // - https://medium.com/androiddevelopers/under-the-hood-of-jetpack-compose-part-2-of-2-37b2c20c6cdd
+    var countInComposable: Int by remember { mutableStateOf(0) }
+    H1 {
+        Text("Counter (a composable)")
+    }
+    Div({ style { padding(25.px) } }) {
+        Button(attrs = {
+            onClick { countInComposable -= 1 }
+        }) {
+            Text("-")
+        }
+
+        Span({ style { padding(15.px) } }) {
+            Text("$countInComposable")
+        }
+
+        Button(attrs = {
+            onClick { countInComposable += 1 }
+        }) {
+            Text("+")
         }
     }
 }
