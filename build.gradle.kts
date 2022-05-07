a59655a5dafe111e957148cf7a4f280281a270df
Piotr Krzeminski <git@krzeminski.it>
2022-05-07 14:33:24 +0200

    Initialize project

---
+++ build.gradle.kts
@@ -0,0 +1,30 @@
+import org.jetbrains.compose.compose
+
+plugins {
+    kotlin("multiplatform") version "1.6.10"
+    id("org.jetbrains.compose") version "1.1.1"
+}
+
+group = "it.krzeminski"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    google()
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+kotlin {
+    js(IR) {
+        browser {}
+        binaries.executable()
+    }
+    sourceSets {
+        val jsMain by getting {
+            dependencies {
+                implementation(compose.web.core)
+                implementation(compose.runtime)
+            }
+        }
+    }
+}
